76249859206f31503e2afb5b98e89f1051186fa1
Aaron Powell <me@aaron-powell.com>
2019-02-01 21:34:29 +1100

    missed an escape character

--- linux/setup.sh
+++ linux/setup.sh
@@ -13,7 +13,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common \
     -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
