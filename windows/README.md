632d117491a1d47b52e42fc5b3a6d9a6f9c0a35d
Aaron Powell <me@aaron-powell.com>
2020-03-25 10:31:49 +1100

    overhaulling the windows setup experience

---
+++ windows/README.md
@@ -0,0 +1,7 @@
+This is for setting up a Windows environment.
+
+Run with:
+
+```powershell
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/aaronpowell/system-init/master/windows/setup.ps1'))
+```
--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,20 +1,17 @@
-function Install-Application {
+function Install-Chocolatey {
+    Set-ExecutionPolicy Bypass -Scope Process -Force;
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+function Install-FromChocolatey {
     param(
         [string]
         [Parameter(Mandatory = $true)]
-        $ApplicationName,
-
-        [ScriptBlock]
-        [Parameter(Mandatory = $true)]
-        $InstallScript
+        $PackageName
     )
 
-    if (!(Get-Command -Name $ApplicationName -ErrorAction SilentlyContinue)) {
-        Write-Host "Installing $ApplicationName"
-        Invoke-Command -ScriptBlock $InstallScript
-    } else {
-        Write-Host "$ApplicationName was already installed, skipping"
-    }
+    choco install $PackageName
 }
 
 function Install-PowerShellModule {
@@ -39,38 +36,18 @@ function Install-PowerShellModule {
     }
 }
 
-Install-Application 'code-insiders' {
-    $outFile = Join-Path $env:TEMP 'vscode-insiders.exe'
-    Invoke-WebRequest -Uri 'https://aka.ms/win32-x64-user-insider' -OutFile $outFile -UseBasicParsing
+Install-Chocolatey
 
-    Start-Process -FilePath $outFile -ArgumentList [[ '/SILENT' ]]
-}
+Install-FromChocolatey 'git'
+Install-FromChocolatey 'vscode-insiders'
+Install-FromChocolatey 'dotnetcore-sdk'
+Install-FromChocolatey 'microsoft-windows-terminal'
+Install-FromChocolatey 'azurestorageemulator'
+Install-FromChocolatey 'fiddler'
+Install-FromChocolatey 'postman'
+Install-FromChocolatey 'linqpad'
+Install-FromChocolatey 'firefox'
+Install-FromChocolatey 'googlechrome'
 
-Install-Application 'git' {
-    $outFile = Join-Path $env:TEMP 'git.exe'
-    Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/download/v2.21.0.windows.1/Git-2.21.0-64-bit.exe' -OutFile $outFile -UseBasicParsing
-
-    Start-Process -FilePath $outFile -ArgumentList [[ '/SILENT' ]]
-
-    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/aaronpowell/system-init/master/common/.gitconfig' -OutFile (Join-Path $env:USERPROFILE '.gitconfig')
-}
-
-Install-Application "C:\Program Files\ConEmu\ConEmu64.exe" {
-    $outFile = Join-Path $env:TEMP 'conemu65.exe'
-    Invoke-WebRequest -Uri 'https://github.com/Maximus5/ConEmu/releases/download/v19.03.10/ConEmuSetup.190310.exe' -OutFile $outFile -UseBasicParsing
-
-    Start-Process -FilePath $outFile -ArgumentList [[ '/SILENT' ]]
-
-    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/aaronpowell/system-init/master/windows/ConEmu.xml' -OutFile (Join-Path $env:APPDATA 'ConEmu.xml')
-}
-
-Install-Application 'go' {
-    $outFile = Join-Path $env:TEMP 'go.msi'
-    Invoke-WebRequest -Uri 'https://dl.google.com/go/go1.12.1.windows-amd64.msi' -OutFile $outFile -UseBasicParsing
-
-    # Start-Process -FilePath $outFile -ArgumentList [[ '/SILENT' ]]
-
-}
-
-Install-Module 'Posh-Git'
-Install-Module 'nvm' { Install-NodeVersion latest }
+Install-Module 'Posh-Git' { Add-PoshGitToProfile -AllHosts }
+Install-Module 'nvm' { Install-NodeVersion latest; Set-NodeVersion -Persist User latest }
